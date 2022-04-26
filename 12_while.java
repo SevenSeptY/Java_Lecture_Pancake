a0ea9ae7d777e5a611a51f44f6eb2f97c8470116
Seven <yaoqi0403@163.com>
2022-04-26 10:22:35 -0400

    Update 12_while.java
    While loop, sum of 1-100 ; ans 5050

--- 12_while.java
+++ 12_while.java
@@ -1,1 +1,23 @@
-// while(expreission){
+/**
+while(expreission){
+ //
+}
+
+While loop, sum of 1-100 ; ans 5050
+*/
+package tutorial;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int num = 0;//for sum 
+		int i = 1;
+		while (i <=100) {
+			num += i;
+			i++;
+		}
+		System.out.println(num);
+		
+	}
+}
