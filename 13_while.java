6bf56116e7c09a7da02d24d95f7d675b6620db5d
Seven <yaoqi0403@163.com>
2022-04-26 15:45:40 -0400

    Update 13_while.java

--- 13_while.java
+++ 13_while.java
@@ -14,5 +14,18 @@ public class Main {
 		}
 		System.out.println(num);
 		
+		//Caculate all odd number and ven number and all number %3 = 0
+		int a = 1;
+		while (a<100) {
+			if(a % 2==0) {
+				System.out.println("even nums" + a);
+			}else if(a % 2==1) {
+				System.out.println("odd nums" +a);
+			}
+			if (a % 3==0) {
+				System.out.println("3 %"+ a);
+			}
+			a++;
+		}
 	}
 }
