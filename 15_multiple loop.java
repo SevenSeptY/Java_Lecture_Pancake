3f283f87e12ac34df677c1e1076e60d503719194
Seven <yaoqi0403@163.com>
2022-04-27 14:52:33 -0400

    Update 15_multiple loop.java

--- 15_multiple loop.java
+++ 15_multiple loop.java
@@ -48,6 +48,31 @@ public class Main {
 	}
 }
 
-
+/*
+    *
+   ***
+  *****
+ *******
+*********
+*/
+public class Main {
+	public static void main(String[] args) {
+		
+		for(int i = 1; i <= 5; i++) {
+			//internal loop for column 
+			for(int j=5; j > i ; j--) {
+				System.out.print(" ");
+			}
+			for(int j=1; j < i ; j++) {
+				System.out.print("*");
+			}
+			for(int j=1; j < i ; j++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("*");
+		}
+	}
+}
 
 
