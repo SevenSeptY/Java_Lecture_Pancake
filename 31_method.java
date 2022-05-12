09db59e7184bff8dd4e17f14f20466c74cdd7c33
Seven <yaoqi0403@163.com>
2022-05-12 15:18:31 -0400

    Update 31_method.java

--- 31_method.java
+++ 31_method.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 /*
  creatMothod: 
  Dog dog= new dog();
+ Static: means that the method belongs to the Main class and not an object of the Main class.
+ Void: means that this method does not have a return value. You will learn more about return values later in this chapter
  */
 
 public class Main{
