0257ed8390b9c02df9b5e11c0090a34cf1eb1072
Seven <yaoqi0403@163.com>
2022-05-14 18:54:51 -0400

    Update 33_Value.java

--- 33_Value.java
+++ 33_Value.java
@@ -1,1 +1,38 @@
+package Java_Felix;
 
+public class Value {
+    public static void main(String[] args){
+        int x = 10;
+        method (x);
+        System.out.println("x="+ x);
+    }
+
+    public static void method(int mx){
+        mx = 20;
+    }
+}
+/*
+output x=10;
+ */
+
+//Ref Vlue
+package Java_Felix;
+
+public class Ref {;
+    public static void main(String[] args){
+        Duck d = new Duck();
+        method(d);
+        System.out.println("Duck age "+ d.age);
+    }
+
+    public static void method (Duck duck){
+        duck.age = 5;
+    }
+}
+
+class Duck{
+    int age = 2;
+}
+
+
+//String
