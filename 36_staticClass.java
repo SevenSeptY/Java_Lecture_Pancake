54f031351747ee0df8788b531a1a52f50a6608c0
Seven <yaoqi0403@163.com>
2022-05-16 11:48:02 -0400

    Create 36_staticClass.java

---
+++ 36_staticClass.java
@@ -0,0 +1,23 @@
+/*
+.class file --> Class Loader
+-----------------------------------------------------
+| Method area | Java Stacks | Native Method Stacks|
+| Heap        |   Program Counter Register        |
+-----------------------------------------------------
+Excution Engine | Native interface  -> Native Libraris
+
+A static method is a method that belongs to a class rather than an instance of a class. The method is accessible to every instance of a class,
+but methods defined in an instance are only able to be accessed by that object of a class.
+
+Whan we have common attribute or method, we can define as Static
+
+public static void main(String[] args){
+//Block
+}
+Main method : Maximum access  
+Public : no need to creat object 
+Static:
+Void : no return value
+Main :
+String[] args:
+*/
